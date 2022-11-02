5431f7a109d6f2a9ba80bbd37e1015b7fce8c15a
Claire.Chang <claire.chang@starclinic.com.tw>
2022-11-02 11:40:49 +0800

    feat: migrate from vuex to pinia

--- src/main.js
+++ src/main.js
@@ -7,13 +7,20 @@ import router from './router';
 import App from './App.vue';
 import Loading from 'vue-loading-overlay';
 import 'vue-loading-overlay/dist/vue-loading.css';
-import store from './store';
+import { createPinia } from 'pinia';
+
+const pinia = createPinia();
 
 const app = createApp(App);
 
+app.use(pinia);
 app.use(router);
-app.use(store);
 app.use(VueAxios, axios);
 app.component('Loading', Loading);
+
+// close debug mode in production
+const isDebugMode = process.env.VUE_APP_ENV !== 'production';
+app.config.debug = isDebugMode;
+app.config.devtools = isDebugMode;
 
 app.mount('#app');
